85045471b66faba66c205d62e29d8ef47c8acfa7
Purush Kuppan <purush.kuppan@gmail.com>
2018-10-06 12:51:39 -0500

    react assiginment 2

--- src/App.js
+++ src/App.js
@@ -13,21 +13,27 @@ class App extends Component {
 
     }
 
+    deleteHandler = (event, index) => {
+        const lattext = [...this.state.textVal]
+        lattext.splice(index,1)
+        this.setState({textVal : lattext.join('')})
+    }
+
   render() {
 
         let chararr = "";
         if (this.state.textVal) {
             console.log(this.state.textVal)
             chararr = ( <div> {
-                this.state.textVal.map((p, index) => {
-                  return  <CharComp inletter = {p} />
+                [...this.state.textVal].map((p, index) => {
+                  return  <CharComp inletter = {p} click={(event) => this.deleteHandler(event, index)} />
                 })
             } </div>)
             }
 
     return (
         <div className="App">
-          Enter your input <input type="text" onChange={(event) => this.calculateLength( event)}/>
+          Enter your input <input type="text" onChange={(event) => this.calculateLength( event)} value = {this.state.textVal}/>
             <p>Your Input Length : {this.state.length}</p>
             <Validation textVal={this.state.textVal}/>
             {chararr}
--- src/Validation/CharComponent.js
+++ src/Validation/CharComponent.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const CharComp = (probs) => {
-   return ( <div>
+    const per = {
+        display: 'inline-block',
+        padding: '16px',
+        textAlign: 'center',
+        border: '1px solid green'
+
+}
+   return ( <div style={per} onClick={probs.click}>
         <h6>{probs.inletter}</h6>
     </div>)
 }
